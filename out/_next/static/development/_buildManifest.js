82ecbf7d2098dc3f7332fdd45b882321db8660e1
Belinda Launer <launerb@gmail.com>
2021-03-25 14:48:14 +1100

    blah

--- out/_next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/contact-us":["static\u002Fchunks\u002Fpages\u002Fcontact-us.js"],"/escort-jobs":["static\u002Fchunks\u002Fpages\u002Fescort-jobs.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcontact-us","\u002Fescort-jobs"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
